237723d3cf140dc0a198125c2ac2573f20492797
Jeongmin Lee <clcl6084@gmail.com>
2025-09-20 18:49:54 +0900

    feat: add buttom component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[14px] text-base font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 font-['Pretendard',_sans-serif] tracking-[-0.32px]",
   {
     variants: {
       variant: {
@@ -20,9 +20,11 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        main: "bg-button-main text-white font-semibold hover:bg-button-main-hover",
+        sub: "bg-button-sub text-button-sub-foreground font-medium hover:bg-button-sub-hover",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-[51px] px-4 py-4",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
